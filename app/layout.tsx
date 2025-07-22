940c4ee92c7b0f5c8a76a6709473aeb2d2d5afa2
Nuiche <94928920+Nuiche@users.noreply.github.com>
2025-07-22 01:41:15 -0400

    Add PWA manifest & SW caching

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-XXXXXXXXXX');
+            gtag('config', 'GG-5X89FD51KS');
           `}
         </Script>
       </head>
