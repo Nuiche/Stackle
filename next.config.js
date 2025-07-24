fdc2900de6c9d3eb5f1dafe5f861d66519c3f44b
Nuiche <94928920+Nuiche@users.noreply.github.com>
2025-07-24 18:27:54 -0400

    Ensure Ga4

--- next.config.js
+++ next.config.js
@@ -4,16 +4,31 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   register: true,
   skipWaiting: true,
   runtimeCaching: [
+    // Don't cache GA script
+    {
+      urlPattern: /^https:\/\/www\.googletagmanager\.com\/gtag\/js?.*$/,
+      handler: 'NetworkOnly',
+      options: { cacheName: 'no-cache' },
+    },
+    // Don't cache GA collect endpoint
+    {
+      urlPattern: /^https:\/\/www\.google-analytics\.com\/g\/collect?.*$/,
+      handler: 'NetworkOnly',
+      options: { cacheName: 'no-cache' },
+    },
+    // API routes
     {
       urlPattern: /^\/api\/.*$/i,
       handler: 'NetworkFirst',
       options: { cacheName: 'api-cache' },
     },
+    // images
     {
       urlPattern: /\.(?:png|jpg|jpeg|svg|ico)$/i,
       handler: 'CacheFirst',
       options: { cacheName: 'image-cache', expiration: { maxEntries: 60 } },
     },
+    // fallback for everything else
     {
       urlPattern: /.*/i,
       handler: 'StaleWhileRevalidate',
@@ -24,4 +39,4 @@ const withPWA = require('@ducanh2912/next-pwa').default({
 
 module.exports = withPWA({
   eslint: { ignoreDuringBuilds: true },
-})
+});
